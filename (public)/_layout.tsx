29bef1a66a2192b33208e53cdfb2cb2dcbc05d07
Udayraj Sahu <96167329+Udayraj-Sahu@users.noreply.github.com>
2025-05-25 10:55:29 +0530

    Delete (public) directory

--- (public)/_layout.tsx
+++
@@ -1,9 +0,0 @@
-// app/(public)/_layout.tsx
-import { Stack } from 'expo-router';
-import React from 'react';
-
-export default function PublicLayout() {
-  // This Stack is for screens within the (public) group, e.g., login, signup, forgot password
-  // The root _layout.tsx will redirect unauthenticated users here.
-  return <Stack screenOptions={{ headerShown: false }} />;
-}
