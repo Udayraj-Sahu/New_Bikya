7ab148a80f96fd6894aa14116aca749a77506e7f
Udayraj Sahu <96167329+Udayraj-Sahu@users.noreply.github.com>
2025-05-25 10:53:58 +0530

    Delete app/(public) directory

--- app/(public)/_layout.tsx
+++
@@ -1,7 +0,0 @@
-// app/(public)/_layout.tsx
-import { Stack } from 'expo-router';
-import React from 'react';
-
-export default function PublicLayout() {
-  return <Stack screenOptions={{ headerShown: false }} />;
-}
