5ece289172bd1f55031f806e884fb1395e4ba4bf
Udayraj Sahu <96167329+Udayraj-Sahu@users.noreply.github.com>
2025-05-25 10:53:48 +0530

    Delete app/(admin) directory

--- app/(admin)/bikes/add.tsx
+++
@@ -1,7 +0,0 @@
-// app/(admin)/bikes/add.tsx
-import React from 'react';
-import { View, Text } from 'react-native';
-
-export default function AddBikeScreen() {
-  return <View><Text>Add Bike Screen</Text></View>;
-}
